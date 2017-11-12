7368a8441137e4af09aa801c12d4f194aa38474f
tompinn23 <tompinncock@gmail.com>
2017-11-12 21:59:46 +0000

    Woo more build updates

--- build.cmd
+++ build.cmd
@@ -1,21 +1,15 @@
 @echo off
 if defined ProgramFiles(x86) (
 	echo Configuring LLVM
-	cmake -G "Visual Studio 15 2017 Win64" -Thost=x64  -H./llvm -Bbuild
+	cmake -G "Visual Studio 15 2017 Win64" -Thost=x64 -DLLVM_BUILD_TOOLS=false -DLLVM_BUILD_TESTS=false -DLLVM_INCLUDE_TESTS=false -DLLVM_BUILD_EXAMPLES=false -DLLVM_INCLUDE_EXAMPLES=false -H./llvm -Bllvm/build
 	echo Configuring Nilang
 	cmake -H. -Bbuild
 ) else (
 	echo Configuring LLVM
-	cmake -H./llvm -Bbuild
+	cmake -DLLVM_BUILD_TOOLS=false -DLLVM_BUILD_TESTS=false -DLLVM_INCLUDE_TESTS=false -DLLVM_BUILD_EXAMPLES=false -DLLVM_INCLUDE_EXAMPLES=false -H./llvm -Bbuild
 	echo Configuring Nilang
 	cmake -H. -Bbuild
 )
 
-set /p ch="Ready To Build [Y/N]"
-set res=false
-if %ch% == Y set res=true
-if %ch% == y set res=true
-if %res% == "true" (
-	cmake --build build
-)
+cmake --build build
 @echo on
